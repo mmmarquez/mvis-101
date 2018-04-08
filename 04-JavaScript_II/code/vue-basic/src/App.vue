d10c67becd3ed76ed406897d82d8cb177be5ce31
Mateo Marquez <mateomarquez@Mateos-MacBook-Pro.local>
2018-04-08 15:10:59 -0400

    Vue Updates

--- 04-JavaScript_II/code/vue-basic/src/App.vue
+++ 04-JavaScript_II/code/vue-basic/src/App.vue
@@ -4,44 +4,13 @@
   </div>
 </template>
 
-<script lang="js">
-
-console.log('hey')
-
-const someName = (x) => {
-
-  console.log(x)
-  // document.write("<h1>Hello World!</h1><p>Have a nice day!</p>");
-
-}
-
-function someFunction() {
-
-console.log(this)
-
-
-}
-
-someName(10)
-someFunction()
-
-
-let someArray = [1, 2, 3, 4, 5, 6]
-someArray.forEach((x, index) => {
-  console.log('value:: ' + x, 'index: ' + index)
-})
-
-let someObject = { name: 'mateo', lastname: 'marquez', city: 'baltimore', state: 'md' }
-
-
-
-for (let number = 0; number <= 12; number = number + 2) {
-
-  console.log(number);
-
-}
-
-
+<script>
+export default {
+  mounted() {
+    // all JS will go here.
+    console.log("hm");
+  }
+};
 </script>
 
 <style lang="css">
